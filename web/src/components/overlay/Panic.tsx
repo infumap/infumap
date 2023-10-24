c768c3df80e184268dd0d0fc5eb5214934c47ba9
infumap <dev@infumap.com>
2023-10-24 15:08:31 +0700

    perfect panic page

--- web/src/components/overlay/Panic.tsx
+++ web/src/components/overlay/Panic.tsx
@@ -22,16 +22,31 @@ import { getPanickedMessage } from "../../util/lang";
 
 
 export const Panic: Component = () => {
+  const eatMouseEvent = (ev: MouseEvent) => { ev.stopPropagation(); }
+  const eatTouchEvent = (ev: TouchEvent) => { ev.stopPropagation(); }
+  const reload = (ev: MouseEvent) => { ev.preventDefault(); window.location = window.location; }
+
   return (
     <div id="panicOverlay"
-         class="absolute left-0 top-0 bottom-0 right-0 outline-none pointer-events-none"
-         style={`background-color: #ff888888; z-index: ${Z_PANIC};`}>
-      <div style="background-color: #ff8888bb; text-align: center; padding: 20px;">
+         class="fixed left-0 top-0 bottom-0 right-0"
+         style={`background-color: #ff000010; z-index: ${Z_PANIC};`}
+         onMouseDown={eatMouseEvent}
+         onMouseUp={eatMouseEvent}
+         onMouseMove={eatMouseEvent}
+         onDblClick={eatMouseEvent}
+         onTouchStart={eatTouchEvent}
+         onTouchEnd={eatTouchEvent}
+         onTouchMove={eatTouchEvent}
+         onTouchCancel={eatTouchEvent}
+         >
+      <div class="fixed select-text"
+           style="color: #ff0000; border-width: 8px; border-color: #ff0000; border-style: solid; background-color: #000000dd; text-align: center; padding: 5px 20px 20px 20px; width: 100%;">
         <div style="font-size: 64px;">Logic Error</div>
-        <div style="font-size: 20px;">A programmer logic error was encountered - the developers expected this case should never occur - there is a bug in the code.</div>
-        <div style="font-size: 20px;">There is a good chance it is safe to ignore this and <a href="">reload</a> the page.</div>
+        <div style="font-size: 20px;">Something the programmers thought should never happen happened - there is a bug in the code.</div>
+        <div style="font-size: 20px;">There is a good chance it is safe to ignore this and <a style="color: #88f;" href="" onClick={reload}>reload</a> the page to continue.</div>
+        <div style="font-size: 20px;">If the problem persists, try removing lines from the end of your items.json database file.</div>
         <div style="font-size: 20px;">You might consider letting the developers know this happened and provide them with the following message:</div>
-        <div style="font-size: 20px;"><i>{getPanickedMessage()}</i></div>
+        <div style={`font-size: 20px; z-index: ${Z_PANIC+1}`}>{getPanickedMessage()}</div>
       </div>
     </div>
   );
