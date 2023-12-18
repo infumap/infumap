5db2d5966c0307ec5dec1cff2895763882e741c0
infumap <dev@infumap.com>
2023-12-18 18:36:09 +0700

    fix image blockSizePx bug

--- web/src/items/image-item.ts
+++ web/src/items/image-item.ts
@@ -110,16 +110,15 @@ export const ImageFns = {
   },
 
   calcGeometry_Spatial: (image: ImageMeasurable, containerBoundsPx: BoundingBox, containerInnerSizeBl: Dimensions, _parentIsPopup: boolean, emitHitboxes: boolean): ItemGeometry => {
-    const sizeBl = ImageFns.calcSpatialDimensionsBl(image);
     const blockSizePx = {
       w: containerBoundsPx.w / containerInnerSizeBl.w,
       h: containerBoundsPx.h / containerInnerSizeBl.h
     };
     const boundsPx = {
-      x: (image.spatialPositionGr.x / GRID_SIZE) + containerBoundsPx.x,
-      y: (image.spatialPositionGr.y / GRID_SIZE) + containerBoundsPx.y,
-      w: sizeBl.w * blockSizePx.w + ITEM_BORDER_WIDTH_PX,
-      h: sizeBl.h * blockSizePx.h + ITEM_BORDER_WIDTH_PX,
+      x: (image.spatialPositionGr.x / (containerInnerSizeBl.w * GRID_SIZE)) * containerBoundsPx.w + containerBoundsPx.x,
+      y: (image.spatialPositionGr.y / (containerInnerSizeBl.h * GRID_SIZE)) * containerBoundsPx.h + containerBoundsPx.y,
+      w: ImageFns.calcSpatialDimensionsBl(image).w / containerInnerSizeBl.w * containerBoundsPx.w + ITEM_BORDER_WIDTH_PX,
+      h: ImageFns.calcSpatialDimensionsBl(image).h / containerInnerSizeBl.h * containerBoundsPx.h + ITEM_BORDER_WIDTH_PX,
     };
     const innerBoundsPx = zeroBoundingBoxTopLeft(boundsPx);
     return {
