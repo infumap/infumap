66e2af3d6d598b7ae02fbc078108f91d23b87063
infumap <dev@infumap.com>
2024-04-12 18:12:09 +0700

    add flags to sdk

--- infusdk/src/item/flags.rs
+++ infusdk/src/item/flags.rs
@@ -14,5 +14,28 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pub mod item;
-pub mod util;
+use bitflags::bitflags;
+
+
+bitflags! {
+  pub struct  NoteFlags: i64 {
+    const None =           0x000;
+    const Heading3 =       0x001;
+    const ShowCopyIcon =   0x002;
+    const Heading1 =       0x004;
+    const Heading2 =       0x008;
+    const Bullet1 =        0x010;
+    const AlignCenter =    0x020;
+    const AlignRight =     0x040;
+    const AlignJustify =   0x080;
+    const HideBorder =     0x100;
+    const Code       =     0x200;
+  }
+}
+
+bitflags! {
+  pub struct TableFlags: i64 {
+    const None =           0x000;
+    const ShowColHeader =  0x001;
+  }
+}
