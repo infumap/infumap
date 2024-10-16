0d8df5e89a5e6aedfc63d2f34d473fba42769fc7
infumap <dev@infumap.com>
2024-10-16 16:59:12 +0700

    fix incorrect calcGeometry_InCell call

--- web/src/layout/arrange/popup.ts
+++ web/src/layout/arrange/popup.ts
@@ -52,7 +52,7 @@ export function arrangeCellPopup(store: StoreContextModel): VisualElementSignal 
     w: desktopBoundsPx.w * 0.8,
     h: desktopBoundsPx.h * 0.8,
   };
-  let geometry = ItemFns.calcGeometry_InCell(li, cellBoundsPx, false, false, true, PageFns.popupPositioningHasChanged(currentPage), false, false);
+  let geometry = ItemFns.calcGeometry_InCell(li, cellBoundsPx, false, false, false, true, PageFns.popupPositioningHasChanged(currentPage), false, false);
   if (renderAsFixed) {
     geometry.boundsPx.x += store.getCurrentDockWidthPx();
     if (geometry.viewportBoundsPx != null) {
