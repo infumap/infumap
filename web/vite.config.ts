ac1a02016a5d3ba4d54314338223aed100d26c8d
infumap <dev@infumap.com>
2024-10-17 10:11:21 +0700

    changes to allow debug in prod

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,49 +1,56 @@
 // @ts-ignore
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/assets': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-      '/account': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-      '/admin': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-      '/command': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-      '/files': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-      '/add': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true
-      },
-    }
-  },
-  build: {
-    target: 'esnext',
-    outDir: './dist',
-    rollupOptions: {
-      input: {
-        // @ts-ignore
-        main: resolve(__dirname, 'index.html'),
-        // @ts-ignore
-        add: resolve(__dirname, 'add.html'),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  let host = "127.0.0.1";
+  if (env["INFUMAP_DEV_HOST"]) {
+    host = env["INFUMAP_DEV_HOST"];
+  }
+  return {
+    plugins: [solidPlugin()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/assets': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+        '/account': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+        '/admin': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+        '/command': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+        '/files': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+        '/add': {
+          target: `http://${host}:8000`,
+          changeOrigin: true
+        },
+      }
+    },
+    build: {
+      target: 'esnext',
+      outDir: './dist',
+      rollupOptions: {
+        input: {
+          // @ts-ignore
+          main: resolve(__dirname, 'index.html'),
+          // @ts-ignore
+          add: resolve(__dirname, 'add.html'),
+        },
       },
     },
-  },
+  }
 });
