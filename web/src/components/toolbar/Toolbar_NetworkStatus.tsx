8175ce4910368d690c76a5ccba228331b2e1fe00
infumap <dev@infumap.com>
2024-12-02 10:07:12 +0700

    network status hover over tooltip

--- web/src/components/toolbar/Toolbar_NetworkStatus.tsx
+++ web/src/components/toolbar/Toolbar_NetworkStatus.tsx
@@ -16,24 +16,52 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { Component } from "solid-js"
+import { Component, Show } from "solid-js"
 import { useStore } from "../../store/StoreProvider";
 import { NETWORK_STATUS_ERROR, NETWORK_STATUS_IN_PROGRESS } from "../../store/StoreProvider_General";
+import { createBooleanSignal } from "../../util/signals";
 
 
 export const Toolbar_NetworkStatus: Component = () => {
   const store = useStore();
 
+  const hoveringOver = createBooleanSignal(false);
+
   const col = () => {
     const status = store.general.networkStatus.get();
-    if (status == NETWORK_STATUS_ERROR) { return "#9b3333"; }
-    if (status == NETWORK_STATUS_IN_PROGRESS) { return "#9a9b33"; }
-    return "#3b9b33";
-  }
+    if (status == NETWORK_STATUS_ERROR) { return "#bb7373"; }
+    if (status == NETWORK_STATUS_IN_PROGRESS) { return "#babb73"; }
+    return "#7bbb73";
+  };
+
+  const hoverText = () => {
+    const status = store.general.networkStatus.get();
+    if (status == NETWORK_STATUS_ERROR) { return "Network Status: Error"; }
+    if (status == NETWORK_STATUS_IN_PROGRESS) { return "Network Status: In Progress"; }
+    return "Network Status: OK";
+  };
+
+  const handleMouseEnter = () => {
+    hoveringOver.set(true);
+  };
+
+  const handleMouseLeave = () => {
+    hoveringOver.set(false);
+  };
 
   return (
-    <div class="w-[21px] h-[21px] inline-block ml-[7px] mr-[-2px]">
-      <div class={`w-[16px] h-[16px] mt-[7px] rounded`} style={`background-color: ${col()};`} />
+    <div class="w-[21px] h-[21px] inline-block ml-[7px] mr-[-2px] relative">
+      <div class={`w-[19px] h-[19px] mt-[7px] rounded border-slate-500 border`}
+           style={`background-color: ${col()};`}
+           onMouseEnter={handleMouseEnter}
+           onMouseLeave={handleMouseLeave}
+           />
+      <Show when={hoveringOver.get()}>
+        <div class={`absolute rounded border-slate-500 border`}
+             style={'top: 40px; right: -25px; width: 200px; padding-left: 8px; padding-top: 4px; padding-bottom: 4px; padding-right: 8px;'}>
+          {hoverText()}
+        </div>
+      </Show>
     </div>
   );
 }
