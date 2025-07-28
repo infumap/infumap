2995b2a36c0904dcb50b17ec43f94d4a8c4b230c
infumap <dev@infumap.com>
2025-07-28 18:24:08 +0700

    package.json -> type module

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
