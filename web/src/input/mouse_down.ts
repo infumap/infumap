infumap/infumap

commit 8789da6d0afd503ca46fe96ebd01ff048542a871

improve behavior when clicking on nested page in popup

web/src/items/page-item.ts
@@ -104,6 +104,32 @@ export interface PageMeasurable extends ItemTypeMixin, PositionalMixin, XSizable
 
 
 export const PageFns = {
+  findOutermostListPage: (visualElement: VisualElement): VisualElement => {
+    let targetVe = visualElement;
+    let currVe: VisualElement | null = visualElement;
+    while (currVe != null) {
+      if (isPage(currVe.displayItem) && asPageItem(currVe.displayItem).arrangeAlgorithm === ArrangeAlgorithm.List) {
+        targetVe = currVe;
+      }
+      if (currVe.flags & VisualElementFlags.Popup) {
+        break;
+      }
+      currVe = currVe.parentPath ? VesCache.get(currVe.parentPath)?.get() ?? null : null;
+    }
+    return targetVe;
+  },
+
+  switchToOutermostListPageMaybe: (visualElement: VisualElement, store: StoreContextModel): void => {
+    const targetVe = PageFns.findOutermostListPage(visualElement);
+    const targetVeid = VeFns.actualVeidFromVe(targetVe);
+    const currentVeid = store.history.currentPageVeid();
+    if (isPage(targetVe.displayItem) && (targetVeid.itemId !== currentVeid?.itemId || targetVeid.linkIdMaybe !== currentVeid?.linkIdMaybe)) {
+      const focusPath = VeFns.computeFocusPathRelativeToRoot(visualElement, targetVeid);
+      switchToPage(store, targetVeid, true, false, false, focusPath);
+    }
+  },
+
+
 
   /**
    * The absolute top level page.
@@ -729,8 +755,10 @@ export const PageFns = {
     } else {
       const focusPath = VeFns.veToPath(visualElement);
       store.history.setFocus(focusPath);
-      const actualVeid = VeFns.actualVeidFromVe(visualElement);
-      switchToPage(store, actualVeid, true, false, false);
+
+      // Find the outermost list page in the current hierarchy (e.g. if in a popup).
+      // If it's different from the current page, switch to it.
+      PageFns.switchToOutermostListPageMaybe(visualElement, store);
     }
   },
 
@@ -738,8 +766,7 @@ export const PageFns = {
     if (handleListPageLineItemClickMaybe(visualElement, store)) { return; }
     const focusPath = VeFns.veToPath(visualElement);
     store.history.setFocus(focusPath);
-    const actualVeid = VeFns.actualVeidFromVe(visualElement);
-    switchToPage(store, actualVeid, true, false, false);
+    PageFns.switchToOutermostListPageMaybe(visualElement, store);
   },
 
   handleEditTitleClick: (visualElement: VisualElement, store: StoreContextModel): void => {

web/src/input/mouse_up.ts
@@ -100,11 +100,11 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
         ? MouseActionState.get().startWidthBl! * GRID_SIZE != asLinkItem(activeItem).spatialWidthGr
         : MouseActionState.get().startWidthBl! * GRID_SIZE != asXSizableItem(activeItem).spatialWidthGr;
       if (xsized ||
-          (isYSizableItem(activeItem) && MouseActionState.get().startHeightBl! * GRID_SIZE != asYSizableItem(activeItem).spatialHeightGr) ||
-          // TODO (LOW): don't update if there are no changes.
-          (isLink(activeItem) && isYSizableItem(activeVisualElement.displayItem)) ||
-          isFlipCard(activeItem) ||
-          (isLink(activeItem) && isFlipCard(activeVisualElement.displayItem))) {
+        (isYSizableItem(activeItem) && MouseActionState.get().startHeightBl! * GRID_SIZE != asYSizableItem(activeItem).spatialHeightGr) ||
+        // TODO (LOW): don't update if there are no changes.
+        (isLink(activeItem) && isYSizableItem(activeVisualElement.displayItem)) ||
+        isFlipCard(activeItem) ||
+        (isLink(activeItem) && isFlipCard(activeVisualElement.displayItem))) {
         serverOrRemote.updateItem(itemState.get(activeItem.id)!, store.general.networkStatus);
       }
       // mouseActionState.activeVisualElement.update(ve => {
@@ -169,7 +169,7 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
             store.overlay.selectedVeids.set([]);
             fullArrange(store);
           }
-      }
+        }
       }
 
       if (MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.AnchorChild) {
@@ -181,8 +181,8 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
         PageFns.handleAnchorDefaultClick(activeVisualElement, store);
 
       } else if (isLink(activeVisualElement.displayItem) &&
-          asLinkItem(activeVisualElement.displayItem).linkRequiresRemoteLogin &&
-          !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TriangleLinkSettings)) {
+        asLinkItem(activeVisualElement.displayItem).linkRequiresRemoteLogin &&
+        !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TriangleLinkSettings)) {
         DoubleClickState.preventDoubleClick();
         const linkItem = asLinkItem(activeVisualElement.displayItem);
         const linkFocusPath = VeFns.addVeidToPath(
@@ -196,8 +196,8 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
         });
 
       } else if (isLink(activeVisualElement.displayItem) &&
-                 !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TriangleLinkSettings) &&
-                 ClickState.getLinkWasClicked()) {
+        !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TriangleLinkSettings) &&
+        ClickState.getLinkWasClicked()) {
         const linkItem = asLinkItem(activeVisualElement.displayItem);
         const linkFocusPath = VeFns.addVeidToPath(
           { itemId: linkItem.id, linkIdMaybe: null },
@@ -217,7 +217,7 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
         store.history.setFocus(focusPath);
 
       } else if ((MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.Flip) ||
-                 (MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TimedFlip)) {
+        (MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.TimedFlip)) {
         DoubleClickState.preventDoubleClick();
         const veid = VeFns.veidFromPath(MouseActionState.get().activeElementPath);
         store.perItem.setFlipCardVisibleSide(veid, store.perItem.getFlipCardVisibleSide(veid) == 0 ? 1 : 0);
@@ -272,15 +272,15 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
         PageFns.handleShiftLeftClick(activeVisualElement, store);
 
       } else if (veFlagIsRoot(VesCache.get(MouseActionState.get().activeRoot)!.get().flags & VisualElementFlags.EmbeddedInteractiveRoot) &&
-                 !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.Move)) {
+        !(MouseActionState.get().hitboxTypeOnMouseDown! & HitboxFlags.Move)) {
         DoubleClickState.preventDoubleClick();
         ItemFns.handleClick(activeVisualElementSignal, MouseActionState.get().hitMeta, MouseActionState.get().hitboxTypeOnMouseDown, store);
 
       } else if (veFlagIsRoot(VesCache.get(MouseActionState.get().activeRoot)!.get().flags) &&
-                 !(VesCache.get(MouseActionState.get().activeRoot)!.get().flags & VisualElementFlags.IsDock) &&
-                 ((VeFns.veidFromVe(VesCache.get(MouseActionState.get().activeRoot)!.get()).itemId != store.history.currentPageVeid()!.itemId) ||
-                  (VeFns.veidFromVe(VesCache.get(MouseActionState.get().activeRoot)!.get()).linkIdMaybe != store.history.currentPageVeid()!.linkIdMaybe)) &&
-                 (CursorEventState.getLatestDesktopPx(store).y > 0)) {
+        !(VesCache.get(MouseActionState.get().activeRoot)!.get().flags & VisualElementFlags.IsDock) &&
+        ((VeFns.veidFromVe(VesCache.get(MouseActionState.get().activeRoot)!.get()).itemId != store.history.currentPageVeid()!.itemId) ||
+          (VeFns.veidFromVe(VesCache.get(MouseActionState.get().activeRoot)!.get()).linkIdMaybe != store.history.currentPageVeid()!.linkIdMaybe)) &&
+        (CursorEventState.getLatestDesktopPx(store).y > 0)) {
         DoubleClickState.preventDoubleClick();
         store.history.setFocus(MouseActionState.get().activeElementPath);
 
@@ -292,6 +292,11 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
           if (selectedVeid == EMPTY_VEID) {
             PageFns.setDefaultListPageSelectedItemMaybe(store, focusPageActualVeid);
           }
+
+          // If we clicked on a root element that is not the current page (e.g. a popup background
+          // or a nested list page), handle potential switching of the root page.
+          // Nested list pages in popups should make the outermost list page the root.
+          PageFns.switchToOutermostListPageMaybe(focusPageVe, store);
         }
 
         // console.log("(1) setting focus to", MouseActionState.get().activeElementPath);
@@ -316,11 +321,18 @@ export function mouseUpHandler(store: StoreContextModel): MouseEventActionFlags 
 
           {
             const focusPagePath = store.history.getFocusPath();
-            const focusPageVe = VesCache.get(focusPagePath)!.get();
-            const focusPageActualVeid = VeFns.veidFromItems(focusPageVe.displayItem, focusPageVe.actualLinkItemMaybe);
-            const selectedVeid = store.perItem.getSelectedListPageItem(focusPageActualVeid);
-            if (selectedVeid == EMPTY_VEID) {
-              PageFns.setDefaultListPageSelectedItemMaybe(store, focusPageActualVeid);
+            const focusPageSignal = VesCache.get(focusPagePath);
+            if (focusPageSignal) {
+              const focusPageVe = focusPageSignal.get();
+              const focusPageActualVeid = VeFns.veidFromItems(focusPageVe.displayItem, focusPageVe.actualLinkItemMaybe);
+              const selectedVeid = store.perItem.getSelectedListPageItem(focusPageActualVeid);
+              if (selectedVeid == EMPTY_VEID) {
+                PageFns.setDefaultListPageSelectedItemMaybe(store, focusPageActualVeid);
+              }
+
+              // If we clicked on an item that is not a root (e.g. a note), handle potential switching 
+              // of the root page to the outermost list page in its hierarchy.
+              PageFns.switchToOutermostListPageMaybe(focusPageVe, store);
             }
           }
 
@@ -401,7 +413,7 @@ function mouseUpHandler_moving_groupAware(store: StoreContextModel, activeItem: 
         }
         activeItem.spatialPositionGr = { x: 0.0, y: 0.0 };
         itemState.moveToNewParent(activeItem, targetPageItem.id, RelationshipToParent.Child);
-         persistMovedItems(store, [activeItem.id]);
+        persistMovedItems(store, [activeItem.id]);
         finalizeMouseUp(store);
         MouseActionState.set(null);
         fullArrange(store);
@@ -421,24 +433,24 @@ function mouseUpHandler_moving_groupAware(store: StoreContextModel, activeItem: 
   if (isPage(overContainerVe.displayItem)) {
     const pageItem = asPageItem(overContainerVe.displayItem);
     if (overContainerVe.flags & VisualElementFlags.IsDock) {
-    const ip = store.perVe.getMoveOverIndexAndPosition(VeFns.veToPath(overContainerVe));
-    activeItem.ordering = itemState.newOrderingAtChildrenPosition(pageItem.id, ip.index, activeItem.id);
-    itemState.sortChildren(pageItem.id);
-   persistMovedItems(store, [activeItem.id]);
+      const ip = store.perVe.getMoveOverIndexAndPosition(VeFns.veToPath(overContainerVe));
+      activeItem.ordering = itemState.newOrderingAtChildrenPosition(pageItem.id, ip.index, activeItem.id);
+      itemState.sortChildren(pageItem.id);
+      persistMovedItems(store, [activeItem.id]);
     }
     else if (pageItem.arrangeAlgorithm == ArrangeAlgorithm.Grid ||
-             pageItem.arrangeAlgorithm == ArrangeAlgorithm.List ||
-             pageItem.arrangeAlgorithm == ArrangeAlgorithm.Justified) {
+      pageItem.arrangeAlgorithm == ArrangeAlgorithm.List ||
+      pageItem.arrangeAlgorithm == ArrangeAlgorithm.Justified) {
       const path = VeFns.veToPath(overContainerVe);
       const idx = store.perVe.getMoveOverIndex(path);
       const insertIndex = pageItem.orderChildrenBy != "" ? 0 : idx;
       activeItem.ordering = itemState.newOrderingAtChildrenPosition(pageItem.id, insertIndex, activeItem.id);
       itemState.sortChildren(pageItem.id);
-     persistMovedItems(store, [activeItem.id]);
+      persistMovedItems(store, [activeItem.id]);
     } else if (pageItem.arrangeAlgorithm == ArrangeAlgorithm.SpatialStretch) {
       if (MouseActionState.get().startPosBl!.x * GRID_SIZE != activeItem.spatialPositionGr.x ||
-          MouseActionState.get().startPosBl!.y * GRID_SIZE != activeItem.spatialPositionGr.y) {
-     persistMovedItems(store, [activeItem.id]);
+        MouseActionState.get().startPosBl!.y * GRID_SIZE != activeItem.spatialPositionGr.y) {
+        persistMovedItems(store, [activeItem.id]);
       }
     } else if (pageItem.arrangeAlgorithm == ArrangeAlgorithm.Calendar) {
       const path = VeFns.veToPath(overContainerVe);
@@ -535,7 +547,7 @@ async function mouseUpHandler_moving_hitboxAttachToComposite(store: StoreContext
       MouseActionState.get().startCompositeItem = null;
     }
 
-  // case #2: attaching to an item that is not inside an existing composite.
+    // case #2: attaching to an item that is not inside an existing composite.
   } else {
 
     // case #2.1: this item is not a composite either.
@@ -693,7 +705,7 @@ function mouseUpHandler_moving_toTable_attachmentCell(store: StoreContextModel, 
   }
 
   const numPlaceholdersToCreate = insertPosition > displayedChild.computed_attachments.length ? insertPosition - displayedChild.computed_attachments.length : 0;
-  for (let i=0; i<numPlaceholdersToCreate; ++i) {
+  for (let i = 0; i < numPlaceholdersToCreate; ++i) {
     const placeholderItem = PlaceholderFns.create(activeItem.ownerId, displayedChild.id, RelationshipToParent.Attachment, itemState.newOrderingAtEndOfAttachments(displayedChild.id));
     itemState.add(placeholderItem);
     server.addItem(placeholderItem, null, store.general.networkStatus);
@@ -831,7 +843,7 @@ function cleanupAndPersistPlaceholders(store: StoreContextModel) {
   while (true) {
     const attachments = placeholderParent.computed_attachments;
     if (attachments.length == 0) { break; }
-    const attachmentId = placeholderParent.computed_attachments[placeholderParent.computed_attachments.length-1];
+    const attachmentId = placeholderParent.computed_attachments[placeholderParent.computed_attachments.length - 1];
     const attachment = itemState.get(attachmentId)!;
     if (attachment == null) { panic("cleanupAndPersistPlaceholders: no attachment."); }
     if (!isPlaceholder(attachment)) {

web/src/layout/navigation.ts
@@ -24,7 +24,7 @@ import { assert, panic } from "../util/lang";
 import { EMPTY_UID, SOLO_ITEM_HOLDER_PAGE_UID, Uid } from "../util/uid";
 import { fArrange } from "./arrange";
 import { initiateLoadItemMaybe, InitiateLoadResult } from "./load";
-import { VeFns, Veid } from "./visual-element";
+import { VeFns, Veid, VisualElementPath } from "./visual-element";
 import { RelationshipToParent } from "./relationship-to-parent";
 
 
@@ -34,39 +34,37 @@ export function switchToNonPage(store: StoreContextModel, url: string) {
 }
 
 function currentUrl(store: StoreContextModel, overrideItemId: Uid | null): string {
-  const itemId = overrideItemId != null ? overrideItemId : store.history.currentPageVeid()!.itemId;
-  const item = itemState.get(itemId);
+  const currentVeid = store.history.currentPageVeid();
+  const itemId = overrideItemId ?? currentVeid?.itemId;
+  if (!itemId) {
+    return "/";
+  }
 
+  const item = itemState.get(itemId);
   if (item && item.origin != null) {
     const encodedOrigin = encodeURIComponent(item.origin);
     return `/remote/${encodedOrigin}/${itemId}`;
   }
 
   const userMaybe = store.user.getUserMaybe();
-  let url = null;
   if (!userMaybe) {
-    if (overrideItemId != null) {
-      url = `/${overrideItemId}`;
-    } else {
-      url = `/${store.history.currentPageVeid()!.itemId}`;
-    }
-  } else {
-    const user = userMaybe;
-    if (overrideItemId != null) {
-      url = `/${overrideItemId}`;
-    } else {
-      if (store.history.currentPageVeid()!.itemId != user.homePageId) {
-        url = `/${store.history.currentPageVeid()!.itemId}`;
-      } else {
-        if (user.username == ROOT_USERNAME) {
-          url = "/";
-        } else {
-          url = `/${user.username}`;
-        }
-      }
-    }
+    return `/${itemId}`;
   }
-  return url;
+
+  const user = userMaybe;
+  if (overrideItemId != null) {
+    return `/${overrideItemId}`;
+  }
+
+  if (itemId !== user.homePageId) {
+    return `/${itemId}`;
+  }
+
+  if (user.username === ROOT_USERNAME) {
+    return "/";
+  }
+
+  return `/${user.username}`;
 }
 
 export function switchToItem(store: StoreContextModel, itemId: Uid, clearHistory: boolean) {
@@ -87,14 +85,14 @@ export function switchToItem(store: StoreContextModel, itemId: Uid, clearHistory
   store.currentUrlPath.set(url);
 }
 
-export function switchToPage(store: StoreContextModel, pageVeid: Veid, updateHistory: boolean, clearHistory: boolean, replace: boolean) {
+export function switchToPage(store: StoreContextModel, pageVeid: Veid, updateHistory: boolean, clearHistory: boolean, replace: boolean, focusPath?: VisualElementPath) {
   if (clearHistory) {
-    store.history.setHistoryToSinglePage(pageVeid);
+    store.history.setHistoryToSinglePage(pageVeid, focusPath);
   } else {
     if (replace) {
       store.history.popPageVeid();
     }
-    store.history.pushPageVeid(pageVeid);
+    store.history.pushPageVeid(pageVeid, focusPath);
   }
 
   fArrange(store);
@@ -130,22 +128,23 @@ async function navigateToLocalRoot(store: StoreContextModel): Promise<void> {
   }
   if (homePageItem) {
     switchToPage(store, { itemId: user.homePageId, linkIdMaybe: null }, false, true, false);
-    if (user.username == ROOT_USERNAME) {
-      window.history.pushState(null, "", "/");
-    } else {
-      window.history.pushState(null, "", `/${user.username}`);
-    }
   }
 }
 
 export async function navigateBack(store: StoreContextModel): Promise<boolean> {
   if (store.history.currentPopupSpec() != null) {
     store.history.popPopup();
-    const page = asPageItem(itemState.get(store.history.currentPageVeid()!.itemId)!);
-    page.pendingPopupPositionGr = null;
-    page.pendingPopupWidthGr = null;
-    page.pendingCellPopupPositionNorm = null;
-    page.pendingCellPopupWidthNorm = null;
+    const currentPageVeid = store.history.currentPageVeid();
+    if (currentPageVeid) {
+      const pageItem = itemState.get(currentPageVeid.itemId);
+      if (pageItem && isPage(pageItem)) {
+        const page = asPageItem(pageItem);
+        page.pendingPopupPositionGr = null;
+        page.pendingPopupWidthGr = null;
+        page.pendingCellPopupPositionNorm = null;
+        page.pendingCellPopupWidthNorm = null;
+      }
+    }
     fArrange(store);
     return true;
   }
@@ -216,20 +215,20 @@ export async function navigateUp(store: StoreContextModel) {
     }
 
     const parentPageMaybe = itemState.get(parentId);
-    if (parentPageMaybe != null) {
+    if (parentPageMaybe) {
       if (isRemote) {
-        if (parentPageMaybe.origin != currentItem.origin) {
+        if (parentPageMaybe.origin !== currentItem.origin) {
           await navigateToLocalRoot(store);
           navigateUpInProgress = false;
           return;
         }
       }
-      if (isPage(parentPageMaybe) && relationshipToParent == RelationshipToParent.Child) {
+      if (isPage(parentPageMaybe) && relationshipToParent === RelationshipToParent.Child) {
         switchToPage(store, { itemId: parentId, linkIdMaybe: null }, true, true, false);
         navigateUpInProgress = false;
         return;
       } else {
-        parentId = parentPageMaybe!.parentId;
+        parentId = parentPageMaybe.parentId;
         relationshipToParent = parentPageMaybe.relationshipToParent;
         continue;
       }

commit 182adc336ed9f8b84fbd7e05308e4a9dde9c470c

fix click bug introduced in 8789da6d0afd503ca46fe96ebd01ff048542a871

web/src/input/mouse_down.ts
@@ -587,20 +587,22 @@ export async function mouseRightDownHandler(store: StoreContextModel) {
     }
   }
 
-  const changedPages = await navigateBack(store);
-  if (!changedPages) {
-    // Save the focused page before navigating up (when focus is on root)
-    const currentPageVeid = store.history.currentPageVeid();
-    if (currentPageVeid) {
-      const currentPageItem = itemState.get(currentPageVeid.itemId);
-      if (currentPageItem && isPage(currentPageItem) && asPageItem(currentPageItem).arrangeAlgorithm === ArrangeAlgorithm.List) {
-        const focusVes = VesCache.get(focusPath);
-        if (focusVes && isPage(focusVes.get().displayItem)) {
-          const focusedVeid = VeFns.actualVeidFromVe(focusVes.get());
-          store.perItem.setFocusedListPageItem(currentPageVeid, focusedVeid);
-        }
+  // Save focus before navigating (when focus is on root list page).
+  // This needs to happen before navigateBack/navigateUp so the focus is preserved.
+  const currentPageVeid = store.history.currentPageVeid();
+  if (currentPageVeid) {
+    const currentPageItem = itemState.get(currentPageVeid.itemId);
+    if (currentPageItem && isPage(currentPageItem) && asPageItem(currentPageItem).arrangeAlgorithm === ArrangeAlgorithm.List) {
+      const focusVes = VesCache.get(focusPath);
+      if (focusVes && isPage(focusVes.get().displayItem)) {
+        const focusedVeid = VeFns.actualVeidFromVe(focusVes.get());
+        store.perItem.setFocusedListPageItem(currentPageVeid, focusedVeid);
       }
     }
+  }
+
+  const changedPages = await navigateBack(store);
+  if (!changedPages) {
     await navigateUp(store);
   }
 }

web/src/items/page-item.ts
@@ -196,6 +196,17 @@ export const PageFns = {
       } else {
         switchToPage(store, targetVeid, true, false, false, focusPath);
       }
+    } else if (isPage(targetVe.displayItem) && isPage(visualElement.displayItem) &&
+      !(visualElement.flags & VisualElementFlags.ListPageRoot)) {
+      // The outermost list page is already the current page, but we clicked on a nested page
+      // (e.g., a page inside a spatial page displayed in the selectedVes area of the list).
+      // In this case, switch to the clicked page directly.
+      // But don't switch if we clicked on the selectedVes page itself (ListPageRoot) - that
+      // should just receive focus, not become the new root.
+      const clickedVeid = VeFns.actualVeidFromVe(visualElement);
+      if (clickedVeid.itemId !== currentVeid?.itemId || clickedVeid.linkIdMaybe !== currentVeid?.linkIdMaybe) {
+        switchToPage(store, clickedVeid, true, false, false);
+      }
     }
   },
 